ae35186ed01ebd7cec4d3f88f92198443149ca0f
Kristen Houser <kristenehouser@gmail.com>
2018-03-06 21:03:08 +0000

    [created] import script

---
+++ import/import.py
@@ -0,0 +1,17 @@
+import pymongo
+import datetime
+
+from pymongo import MongoClient
+client = MongoClient()
+
+client = MongoClient('localhost', 27017)
+
+db = client.test_db
+collection = db.test_coll
+
+post = {"author": "Kristen",
+	"text": "First post",
+	"date": datetime.datetime.utcnow()}
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+
