cdd21232d79af189645dc7b010f127623ec47b2d
kb3zba <kristenehouser@gmail.com>
2018-03-07 15:20:33 -0500

    [created] driver app

---
+++ import/driver.py
@@ -0,0 +1,20 @@
+import urllib.request as urllib2
+import simplejson
+import pymongo
+import datahubapi
+
+from pymongo import MongoClient
+
+def main():
+	client = MongoClient('localhost', 27017)
+	db = client.geo_attr
+	stations = db.stations
+
+	# import all station geographical attributes
+	geoAttr_stations(client)
+
+	# iterate through all attributes to import all station details
+	for station in stations.find():
+		print(station)
+
+
