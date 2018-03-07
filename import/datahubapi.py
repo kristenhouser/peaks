51b59f6d421cc623d5e8a7bd2b2cea282e8ddd48
kb3zba <kristenehouser@gmail.com>
2018-03-07 14:32:38 -0500

    [added] water quality stations api hook

--- import/datahubapi.py
+++ import/datahubapi.py
@@ -4,6 +4,18 @@ import pymongo
 
 from pymongo import MongoClient
 
+def waterquality_stations():
+	url = "http://datahub.chesapeakebay.net/api.JSON/WaterQuality/Station/"
+	print(url)
+	response = urllib2.urlopen(url)
+	data = simplejson.load(response)
+	print(data)
+	client = client = MongoClient('localhost', 27017)
+	db = client.waterquality 
+	stations = db.stations
+	station_ids = stations.insert_many(data).inserted_ids
+	print(station_ids)
+
 def waterquality_station(geo_attr, attr_id):
 	url = "http://datahub.chesapeakebay.net/api.JSON/WaterQuality/Station/" + geo_attr + "/" + attr_id
 	print(url)
